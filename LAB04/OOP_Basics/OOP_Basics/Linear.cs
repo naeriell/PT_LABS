74cf909d367bf5213a6076be90192d68e625ca0b
MARIA YAKUNINA <mariayakunina7@gmail.com>
2025-11-11 18:27:54 +0300

    Add Task 1 Lab4

---
+++ LAB04/OOP_Basics/OOP_Basics/Linear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP_Basics
+{
+    public class Linear
+    {
+
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public Linear(double a, double b)
+        {
+            if (double.IsNaN(a))
+            {
+                Console.WriteLine("Поле a со значением NaN, запишите верное значение после");
+            }
+            else
+            {
+                this.A = a;
+            }
+
+            this.B = b;
+        }
+
+        public double Calculate(double x)
+        {
+            return A * x + B;
+        }
+
+        public void PrintEquation()
+        {
+            System.Console.WriteLine($"y = {A}x + {B}");
+        }
+    }
+}
---
+++ LAB04/OOP_Basics/OOP_Basics/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP_Basics;
+
+class Program
+{
+    static void Main()
+    {
+        Linear eq1 = new Linear(3, -4);
+        eq1.PrintEquation();
+        Console.WriteLine($"eq1 при x=1: {eq1.Calculate(1)}");
+
+        Linear eq2 = new Linear(0, 7);
+        eq2.PrintEquation();
+        Console.WriteLine($"eq2 при x=-8: {eq2.Calculate(-8)}");
+
+        Linear eq3 = new Linear(-2, 0);
+        eq3.PrintEquation();
+        Console.WriteLine($"eq3 при x=3: {eq3.Calculate(3)}");
+
+
+
+    }
+}
